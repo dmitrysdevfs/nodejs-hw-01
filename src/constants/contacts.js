c7e9c3bade69dc2d6e31fbfe19ef7e83c61d76cf
Dmytro <d.salyakin@gmail.com>
2025-05-03 18:04:30 +0300

    Initial PATH_DB

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,8 @@
-export const PATH_DB =
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const filePath = path.join(dirname, 'db/db.json');
+
+export const PATH_DB = filePath;
