237ea4770e1a9cd542c3a47199e03ab3dc86a22c
Dmytro <d.salyakin@gmail.com>
2025-05-04 04:10:32 +0300

    Implement function to write contacts into file

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,17 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, undefined, 2));
+    return updatedContacts;
+  } catch (error) {
+    console.error('Writing error:', error);
+    return null;
+  }
+};
+
+writeContacts([
+  { id: '1', name: 'John' },
+  { id: '2', name: 'Jane' },
+]);
