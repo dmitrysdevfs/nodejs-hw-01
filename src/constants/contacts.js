97bd1fc495029d5fc787ddc3f9d0ac98aab5ab7f
Dmytro <d.salyakin@gmail.com>
2025-05-04 03:28:47 +0300

    Add function to read contacts from file

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -3,6 +3,6 @@ import { fileURLToPath } from 'node:url';
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
-const filePath = path.join(dirname, 'db/db.json');
+const filePath = path.join(dirname, '../db/db.json');
 
 export const PATH_DB = filePath;
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,14 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const contacts = await fs.readFile(PATH_DB, { encoding: 'utf-8' });
+    return console.log(JSON.parse(contacts));
+  } catch (error) {
+    console.error('Reading error:', error);
+    return [];
+  }
+};
+
+readContacts();
